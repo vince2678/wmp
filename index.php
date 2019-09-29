c09c2169a2fbf38065184c8d14d63c87db1abcc5
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-09-29 12:38:36 -0400

    index: Add skeletal html tags

--- index.php
+++ index.php
@@ -5,3 +5,17 @@ include "db.php";
 include "cookies.php";
 include "api.php";
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+    <head>
+        <meta charset="utf8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    </head>
+
+    <body>
+    </body>
+
+</html>
