a184298fa3c17faf1b8e387181eac6df9e652c06
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-10-03 08:33:15 -0400

    db: Add methods for inserting, updating and deleting rows

--- db.php
+++ db.php
@@ -26,4 +26,75 @@ function connect_to_db()
     return $mysqli;
 }
 
+function insert_row($table, $kv_pair)
+{
+    $db = connect_to_db();
+
+    $k = array_keys($kv_pair);
+
+    for ($i = 0; $i < count($k) - 1; $i++)
+    {
+        $key = $k[$i];
+        $keys .= $db->escape_string($key) . ",";
+        $values .= "'" . $db->escape_string($kv_pair[$key]) . "',";
+    }
+    $key = $k[$i];
+    $keys .= $db->escape_string($key);
+    $values .= "'" . $db->escape_string($kv_pair[$key]) . "'";
+
+    $query = "INSERT INTO " . $db->escape_string($table)
+        . "(" . $keys . ")"
+        . " value(" . $values . ");";
+
+    if (false == ($res = $db->query($query)))
+        printf("Failed to insert: %s\n", $db->error);
+
+    $db->close();
+
+    return $res;
+}
+
+function update_row($table, $key, $value, $kv_pair)
+{
+    $db = connect_to_db();
+
+    $k = array_keys($kv_pair);
+
+    for ($i = 0; $i < count($k) - 1; $i++)
+    {
+        $row .= $db->escape_string($k[$i]) . '='
+            . "'" . $db->escape_string($kv_pair[$k[$i]]) . "',";
+    }
+    $row .= $db->escape_string($k[$i]) . '='
+        . "'" . $db->escape_string($kv_pair[$k[$i]]) . "'";
+
+    $query = "UPDATE " . $db->escape_string($table) . " SET " . $row
+        . " WHERE "
+        . $db->escape_string($key)
+        . "='" . $db->escape_string($value) . "';";
+
+    if (false == ($res = $db->query($query)))
+        printf("Failed to update: %s\n", $db->error);
+
+    $db->close();
+
+    return $res;
+}
+
+function delete_row($table, $key, $value)
+{
+    $db = connect_to_db();
+
+    $query = "DELETE FROM " . $db->escape_string($table) . " WHERE "
+        . $db->escape_string($key) . "="
+        . "'" . $db->escape_string($value) . "';";
+
+    if (false == ($res = $db->query($query)))
+        printf("Failed to delete: %s\n", $db->error);
+
+    $db->close();
+
+    return $res;
+}
+
 ?>
