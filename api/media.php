4d8bd2cb1a1cc0d0bc68835b0fef54bbd842aab0
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-10-10 23:38:59 -0400

    api: media: Define get_raw_media() function
     - Define function to contents of media files given media info

--- api/media.php
+++ api/media.php
@@ -2,11 +2,63 @@
 
 require_once(__DIR__ . "../lib/getid3/getid3.php");
 
-/* Get raw media output
-*/
-function get_raw_media($id)
+/* Get raw media output */
+function get_raw_media($kv_pair)
 {
+    $db = connect_to_db();
 
+    $k = array_keys($kv_pair);
+
+    $constraint = null;
+
+    for ($i = 0; $i < count($k) - 1; $i++)
+    {
+        $constraint .= $db->escape_string($k[$i]) . '='
+            . "'" . $db->escape_string($kv_pair[$k[$i]]) . "' AND ";
+    }
+    $constraint .= $db->escape_string($k[$i]) . '='
+        . "'" . $db->escape_string($kv_pair[$k[$i]]) . "';";
+
+    $query = "SELECT * from library_media WHERE "
+        . $constraint;
+
+    if (false == ($result = $db->query($query)))
+    {
+        printf("Query failed: %s\n", $db->error);
+        return;
+    }
+
+    if (null == ($row = $result->fetch_assoc()))
+    {
+        printf("No rows in query\n");
+        return;
+    }
+
+    $path = $row['full_path'] . "/". $row['relative_path'];
+
+    $finfo = new \finfo(FILEINFO_MIME_TYPE, "/usr/lib/file/magic.mgc");
+
+    $fsize = filesize($path);
+
+    header('Content-Type: ' . $finfo->file($path));
+    header('Content-Length: ' . $fsize);
+
+    if (false == ($fh = fopen($path, "r")))
+    {
+        printf("Failed to open file\n");
+        return;
+    }
+
+    $bs = 256;
+
+    while (!feof($fh))
+    {
+        $data = fread($fh, $bs);
+        echo $data;
+    }
+
+    fclose($fh);
+    $db->close();
 }
 
 /* 
