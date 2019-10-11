444d2af96cbc860832c6a704537ec50d9561d86e
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-10-10 23:14:50 -0400

    api: Define add_genre and get_genre_id functions

--- api/genre.php
+++ api/genre.php
@@ -1,18 +1,47 @@
 <?php
 
+/* add genre and/or return genre_id */
 function add_genre($name)
 {
+    $row = array(
+        "name" => $name,
+    );
 
+    if (count_rows("genre", $row) < 1)
+        return insert_row("genre", $row);
+    //else
+
+    return true;
 }
 
 function get_genre_id($name)
 {
-    return -1;
+    $id = -1;
+
+    $db = connect_to_db();
+
+    $query = "SELECT * from genre WHERE"
+        . " name='"  . $db->escape_string($name) . "';";
+
+    if (false == ($result = $db->query($query)))
+        return $id;
+
+    if (null == ($row = $result->fetch_assoc()))
+        return $id;
+
+    $id = $row['genre_id'];
+
+    $result->free();
+    $db->close();
+
+    return $id;
 }
 
+/*
 function get_genres()
 {
 
 }
+*/
 
 ?>
