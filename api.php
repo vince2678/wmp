00323bd64d1829d7b4218831d03b077da80feeb1
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-10-15 00:31:48 -0400

    api: Include all api/*php files in api.php

--- api.php
+++ api.php
@@ -1,6 +1,13 @@
 <?php
+
+include __DIR__ . "/api/" . "artist.php";
 include __DIR__ . "/api/" . "genre.php";
 include __DIR__ . "/api/" . "library.php";
 include __DIR__ . "/api/" . "playlist.php";
 include __DIR__ . "/api/" . "media.php";
+
+include __DIR__ . "/api/" . "music.php";
+include __DIR__ . "/api/" . "photo.php";
+include __DIR__ . "/api/" . "video.php";
+
 ?>
