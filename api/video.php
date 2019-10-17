03f3ddb0e8b46767dd0c49975f29591498a5a106
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-10-17 13:08:05 -0400

    api: video: Read video metadata when adding new video

--- api/video.php
+++ api/video.php
@@ -9,11 +9,30 @@ function add_video($media_id)
 
     $m_row = get_row("media", $constraint);
 
-    $data = array(
-        "media_id" => $media_id,
-        "title" => basename($m_row['relative_path']),
-        "duration" => 0,
-    );
+    /* fetch metadata */
+    $meta = get_media_metadata($media_id)[$media_id];
+    $tags = $meta['comments'];
+
+    $data["media_id"] = $media_id;
+
+    if (isset($tags['title'][0]))
+        $data['title'] = $tags['title'][0];
+    elseif (isset($m_row['relative_path']))
+        $data['title'] = $m_row['relative_path'];
+
+    if (isset($meta['playtime_seconds']))
+        $data['duration'] = (int) $meta['playtime_seconds'];
+    else
+        $data['duration'] = 0;
+
+    // add genre if not exists
+    if (isset($tags['genre'][0]))
+    {
+        $genre = $tags['genre'][0];
+
+        if (add_genre($genre))
+            $data['genre_id'] = get_genre_id($genre);
+    }
 
     if (count_rows("video", $constraint) < 1)
         return insert_row("video", $data);
