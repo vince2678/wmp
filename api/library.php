43e466a0cbbed3c830a448ff5f45d3595d51d586
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-10-17 16:49:22 -0400

    api: library: Merge scan_library and scan_libraries
     - allow scan of all libraries if id < 0 is passed

--- api/library.php
+++ api/library.php
@@ -15,7 +15,7 @@ function update_library($id, $path, $type, $interval)
 */
 
 /* take in a library $row */
-function scan_library($library_id)
+function scan_library($library_id = -1)
 {
     $mimes['music'] =
         array(
@@ -40,95 +40,85 @@ function scan_library($library_id)
             "image/gif"
         );
 
-    $library = get_row("library", array("library_id" => $library_id));
+    if ($library_id < 0)
+        $constraint = array();
+    else
+        $constraint = array("library_id" => $library_id);
 
-    if (!isset($library))
-        return false;
-
-    $group_mimes = $mimes[$library['type']];
+    $libraries = get_rows("library", $constraint);
 
     $finfo = new \finfo(FILEINFO_MIME_TYPE, "/usr/lib/file/magic.mgc");
 
-    $d_stack = array();
-    $f_stack = array();
-
-    $d_stack[] = $library['full_path'];
-
-    //  add files to file stack
-    while (null !== ($dir = array_pop($d_stack)))
-    {
-        if (false == ($dh = opendir($dir)))
-        {
-            continue;
-        }
-
-        while (false !== ($file = readdir($dh)))
-        {
-            $path = $dir . "/" . $file;
-
-            $ft = filetype($path);
-
-            // dont add hidden files or . and .. entries
-            if (("dir" == $ft) && (0 !== strncmp('.', $file, 1)))
-            {
-                $d_stack[] = $path;
-            }
-            elseif ("file" == $ft)
-            {
-                if (false !== array_search($finfo->file($path), $group_mimes))
-                {
-                    $rpath = substr($path, strlen($library['full_path']));
-
-                    while ($rpath[0] == "/")
-                        $rpath = substr($rpath, 1);
-
-                    $f_stack[] = $rpath;
-                }
-            }
-        }
-        closedir($dh);
-    }
-
-    add_media($library_id, $f_stack);
-
-    return true;
-}
-
-function scan_libraries($constraint)
-{
-    $rows = get_rows("library", $constraint);
-
-    if ($rows == null) //no rows
-        return false;
-
     $db = connect_to_db();
 
-    foreach ($rows as $row)
+    foreach ($libraries as $library)
     {
         $query = "SELECT MAX(last_update) as last FROM media WHERE"
-            . " library_id='" . $row['library_id'] . "';";
+            . " library_id='" . $library['library_id'] . "';";
 
         $result = $db->query($query);
 
         if (isset($result) && $result->num_rows > 0)
-            $m_row = $result->fetch_assoc();
+            $mru_row = $result->fetch_assoc();
 
-        if (isset($m_row['last']))
+        if (isset($mru_row['last']))
         {
             $current_time = new \DateTime();
-            $update_time = new \DateTime($m_row['last']);
+            $update_time = new \DateTime($mru_row['last']);
             $diff = $current_time->getTimestamp() - $update_time->getTimestamp();
 
             // don't rescan if the update interval hasn't passed yet
-            if ($diff < $row['update_interval'])
+            if ($diff < $library['update_interval'])
+            {
+                $result->free();
                 continue;
+            }
         }
-
-        scan_library($row['library_id']);
-        clean_media_records($row['library_id']);
-
         $result->free();
+
+        $d_stack = array();
+        $f_stack = array();
+
+        $d_stack[] = $library['full_path'];
+
+        //  add files to file stack
+        while (null !== ($dir = array_pop($d_stack)))
+        {
+            if (false == ($dh = opendir($dir)))
+            {
+                continue;
+            }
+
+            while (false !== ($file = readdir($dh)))
+            {
+                $path = $dir . "/" . $file;
+
+                $ft = filetype($path);
+
+                // dont add hidden files or . and .. entries
+                if (("dir" == $ft) && (0 !== strncmp('.', $file, 1)))
+                {
+                    $d_stack[] = $path;
+                }
+                elseif ("file" == $ft)
+                {
+                    if (false !== array_search($finfo->file($path), $mimes[$library['type']]))
+                    {
+                        $rpath = substr($path, strlen($library['full_path']));
+
+                        while ($rpath[0] == "/")
+                            $rpath = substr($rpath, 1);
+
+                        $f_stack[] = $rpath;
+                    }
+                }
+            }
+            closedir($dh);
+        }
+        add_media($library['library_id'], $f_stack);
+        clean_media_records($library['library_id']);
     }
+
     $db->close();
 
     return true;
@@ -137,18 +127,15 @@ function scan_libraries($constraint)
 function library_url_handler($data)
 {
 
-    $constraint = array();
+    $library_id = -1;
 
     switch($data['column'])
     {
         case 'id':
         {
             if (isset($data['value']) && ("" !== $data['value']))
-            {
-                $constraint = array(
-                    $data['table'] . "_id" => $data['value'],
-                );
-            }
+                $library_id = $data['value'];
+
             break;
         }
         case 'name':
@@ -158,6 +145,14 @@ function library_url_handler($data)
                 $constraint = array(
                     $data['column'] => htmlspecialchars_decode($data['value']),
                 );
+
+                if (null !== $row = get_row("library", $constraint))
+                    $library_id = $row['library_id'];
+                else
+                {
+                    echo "No library with name " . $data['value'] . " found\n";
+                    die();
+                }
             }
             break;
         }
@@ -175,7 +170,8 @@ function library_url_handler($data)
         case 'scan':
         {
             header("Content-Type: application/json");
-            if(scan_libraries($constraint))
+
+            if(scan_library($library_id))
                 echo '{"status" : "success"}' . PHP_EOL;
             else
                 echo '{"status" : "failure",'
