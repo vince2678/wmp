fdb06961e5f73061796bf3aa8e9a8969a0d67371
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-10-17 22:32:25 -0400

    api: playlist: Implement playlist functions
     - implement functions for adding, updating playlists and adding
       and removing tracks from playlists

--- api/playlist.php
+++ api/playlist.php
@@ -1,20 +1,58 @@
 <?php
 
-function create_playlist($name)
+/*
+Add playlist with $name, duplicate names allowed
+*/
+function add_playlist($name)
 {
-
+    $data = array("name" => $name);
+    return insert_row("playlist", $data);
 }
 
 function update_playlist($id, $name)
 {
+    $constraint = array("playlist_id" => $id);
+    $data = array("name" => $name);
 
+    return update_row("playlist", $constraint, $data);
 }
 
-/*
-function add_track_to_playlist($playlist_id, $media_id)
+function add_playlist_media($playlist_id, $media_id, $rank = NULL)
 {
+    $data = array(
+        "media_id" => $media_id,
+        "playlist_id" => $playlist_id,
+    );
 
+    $count = count_rows("playlist_media", $data);
+
+    if (isset($rank))
+        $data['rank'] = $rank;
+
+    if ($count == 0)
+        return insert_row("playlist_media", $data);
+    elseif ($count > 0)
+    {
+        $constraints = array(
+            "media_id" => $media_id,
+            "playlist_id" => $playlist_id,
+        );
+
+        return update_row("playlist_media", $constraints, $data);
+    }
+    /*else //($count < 0)*/
+
+    return false;
 }
-*/
+
+function remove_playlist_media($playlist_id, $media_id)
+{
+    $constraints = array(
+        "media_id" => $media_id,
+        "playlist_id" => $playlist_id,
+    );
+
+    return delete_row("playlist_media", $constraints);
+}
 
 ?>
