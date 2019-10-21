ed361c43445e0aadcc506bc2460d83cbdec5ba7b
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-10-21 14:03:13 -0400

    js: Add function to format time as [hh:]mm:ss

--- js/misc.js
+++ js/misc.js
@@ -7,3 +7,30 @@ function clearChildren(object)
         object.removeChild(object.lastChild);
     }
 }
+
+function formatTime(seconds)
+{
+    var hours, minutes;
+    var ret = "";
+
+    hours = parseInt(seconds/3600);
+    seconds = seconds % 3600;
+
+    minutes = parseInt(seconds/60);
+    seconds = seconds % 60;
+
+    if (hours > 0)
+        ret = ret + hours + ":";
+
+    if (minutes > 9)
+        ret = ret + minutes + ":";
+    else
+        ret = ret + 0 + minutes + ":";
+
+    if (seconds > 9)
+        ret = ret + seconds;
+    else
+        ret = ret + 0 + seconds;
+
+    return ret;
+}
