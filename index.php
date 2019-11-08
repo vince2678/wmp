67cac40a0e0a306c3bf43aad526153e1630c45b1
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-11-08 00:34:56 -0500

    Move queue-related js functions to queue.js

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ include "theme.php";
         <?php import_theme() ?>
         <script src='js/globals.js'></script>
         <script src='js/misc.js'></script>
+        <script src='js/queue.js'></script>
         <script src='js/ajax.js'></script>
         <script src='js/interface.js' type='module'></script>
     </head>
---
+++ js/queue.js
@@ -0,0 +1,102 @@
+'use strict';
+
+function getNewShuffle()
+{
+    var shuffle = [];
+    var queue = global_player_state['queue'];
+    var visited = new Set();
+
+    while (shuffle.length < queue.length)
+    {
+        let index = Math.floor(Math.random() * queue.length);
+
+        if (visited.has(index))
+            continue;
+
+        visited.add(index);
+        shuffle.push(queue[index]);
+    }
+
+    return shuffle;
+}
+
+function getMediaQueue(group, value)
+{
+    var media = global_player_state['media'];
+
+    var queue = [];
+    var type = null;
+
+    function filterMedia(constraint) {
+        for (let medium of media)
+        {
+            let match = true;
+
+            for (let key in constraint)
+            {
+                if (medium[key] != constraint[key])
+                {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match)
+                queue.push(medium['media_id']);
+        }
+    }
+
+    switch(group)
+    {
+        case 'playlist':
+        {
+            let playlist = global_player_state['playlist'].filter(
+                function(e, i, a)
+                { return (e['playlist_id'] == value); }
+            )[0];
+
+            if (playlist['playlist_id'] !== undefined)
+                type = playlist['type'];
+
+            let playlist_media = global_player_state['playlist_media'].filter(
+                function(e, i, a)
+                { return (e['playlist_id'] == value); }
+            );
+
+            //TODO: Incorporate rank into table ordering
+            for (let playlist_item of playlist_media)
+            {
+                queue.push(playlist_item['media_id']);
+            }
+
+            break;
+        }
+        case 'library':
+        {
+            let library = global_player_state['library'].filter(
+                function(e, i, a)
+                { return (e['library_id'] == value); }
+            )[0];
+
+
+            if (library['library_id'] !== undefined)
+                type = library['type'];
+
+            filterMedia({'library_id':value});
+            break;
+        }
+        case 'type':
+        {
+            filterMedia({'library_type':value});
+            type = value;
+            break;
+        }
+        default:
+        {
+            console.log("Unknown group selected");
+            break;
+        }
+    }
+
+    return {'queue': queue, 'type': type};
+}
