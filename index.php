37ea8bd3bc1df5f27d74ccc9ba1f373d28eb047b
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-11-08 13:23:29 -0500

    js: Move resizeElems to interface.js

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ include "theme.php";
         <script src='js/media_player.js'></script>
         <script src='js/content.js'></script>
         <script src='js/navigation.js'></script>
+        <script src='js/interface.js'></script>
         <script src='js/ajax.js'></script>
         <script src='js/main.js' type='module'></script>
     </head>
--- js/main.js
+++ js/main.js
@@ -104,49 +104,6 @@ function toggleMediaPlayerSize(target = undefined)
     resizeElems();
 }
 
-function resizeElems()
-{
-    var top_nav = document.querySelector('#top_nav');
-    var left_nav = document.querySelector('#left_nav');
-
-    var media_content = document.querySelector('#content');
-    var media_player = document.querySelector('#media_player');
-    var media_preview = document.querySelector('#media_player #content_preview');
-
-    left_nav.style.height = (window.innerHeight - top_nav.offsetHeight - media_player.offsetHeight) + 'px';
-
-    media_content.style.width = (window.innerWidth - left_nav.offsetWidth) + 'px';
-    media_content.style.height = left_nav.style.height;
-
-    switch(media_player.className)
-    {
-        case PLAYER_SIZE_SMALL:
-        {
-            let seek_bar = document.querySelector('#media_player #seek_bar');
-
-            /* set 16:9 ratio for preview dimensions and make clearance for seek bar on height */
-            let preview_height = parseInt(media_player.clientHeight - (2 * seek_bar.clientHeight));
-            let preview_width = parseInt(preview_height / 9 * 16);
-
-            media_preview.style.height =  preview_height + 'px';
-            media_preview.style.width = preview_width + 'px';
-            break;
-        }
-        case PLAYER_SIZE_FULL:
-        {
-            media_preview.style.height = (window.innerHeight - media_player.clientHeight) + 'px';
-            media_preview.style.width = '100%';
-            break;
-        }
-        default:
-        {
-            media_preview.style.height = media_content.clientHeight + 'px';
-            media_preview.style.width = media_content.clientWidth + 'px';
-            break;
-        }
-    }
-}
-
 /* populate the media and library sections */
 function populateMediaLibraries()
 {
