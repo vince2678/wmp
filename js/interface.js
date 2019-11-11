193127331fafd9b6569c7ac7702f447b3f7cece7
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2019-11-10 23:37:31 -0500

    js: Move toggleMediaPlayerSize to interface.js

--- js/interface.js
+++ js/interface.js
@@ -42,3 +42,107 @@ function resizeElems()
         }
     }
 }
+
+function toggleMediaPlayerSize(target = undefined)
+{
+    var media_player = document.querySelector('#media_player');
+    var media_preview = document.querySelector('#media_player #content_preview');
+
+    let all = [ PLAYER_SIZE_SMALL, PLAYER_SIZE_NORMAL, PLAYER_SIZE_WIDE, PLAYER_SIZE_LARGE, PLAYER_SIZE_FULL ];
+
+    let cycle = [ PLAYER_SIZE_SMALL, PLAYER_SIZE_LARGE, PLAYER_SIZE_FULL ];
+
+    let index;
+
+    if (target)
+    {
+        console.log("Target specified: " + target);
+    }
+    else if ((index = cycle.indexOf(media_player.className)) > -1)
+    {
+        let targetIndex = (index - 1);
+
+        if (targetIndex < 0)
+            target = PLAYER_SIZE_HIDDEN;
+        else
+            target = cycle[targetIndex];
+    }
+    else if (all.indexOf(media_player.className) > -1)
+    {
+        target = PLAYER_SIZE_SMALL;
+    }
+
+    let fullscreen_icon = document.querySelector('#media_player #media_fullscreen i');
+    let resize_icon = document.querySelector('#media_player #media_resize i');
+
+    switch(target)
+    {
+        case PLAYER_SIZE_FULL:
+        {
+            try
+            {
+                if (!isFullScreen())
+                    document.querySelector('body').requestFullscreen();
+            }
+            catch(err)
+            {
+                console.log("Failed to set fullscreen: " + err.message);
+            }
+
+            closeLeftNav();
+            closeTopNav();
+
+            resize_icon.innerHTML = 'keyboard_arrow_down';
+            fullscreen_icon.innerHTML = 'fullscreen_exit';
+
+            break;
+        }
+        case PLAYER_SIZE_LARGE:
+        {
+            if (isFullScreen())
+                document.exitFullscreen();
+
+            closeLeftNav();
+            closeTopNav();
+
+            resize_icon.innerHTML = 'keyboard_arrow_down';
+            fullscreen_icon.innerHTML = 'fullscreen';
+
+            break;
+        }
+        case PLAYER_SIZE_WIDE:
+        {
+            closeLeftNav();
+            openTopNav();
+
+            resize_icon.innerHTML = 'keyboard_arrow_down';
+            fullscreen_icon.innerHTML = 'fullscreen';
+
+            break;
+        }
+        case PLAYER_SIZE_HIDDEN:
+        {
+            stopMediaPlayback();
+        }
+        case PLAYER_SIZE_NORMAL:
+        case PLAYER_SIZE_SMALL:
+        {
+            openLeftNav();
+            openTopNav();
+
+            resize_icon.innerHTML = 'close';
+            fullscreen_icon.innerHTML = 'fullscreen';
+
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+
+    media_player.className = target;
+    media_preview.className = target;
+
+    resizeElems();
+}
